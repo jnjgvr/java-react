c63d01f020f07e600bcecfe741a55fc5567583d8
jnjgvr <jnjgvr@gmail.com>
2025-03-14 16:30:01 +0100

    Actividad Math

---
+++ js-math/main.js
@@ -0,0 +1,4 @@
+var ejercicio1 = Math.round(Math.random());
+console.log(ejercicio1);
+var ejercicio2 = Math.round(Math.PI);
+console.log(ejercicio2);
---
+++ js-math/main.ts
@@ -0,0 +1,5 @@
+let ejercicio1 : number = Math.round(Math.random());
+console.log(ejercicio1);
+
+let ejercicio2 = Math.round(Math.PI);
+console.log(ejercicio2);
