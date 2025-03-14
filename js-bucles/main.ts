9c1c7472757b51ac13e53e52624de4f35ada1839
jnjgvr <jnjgvr@gmail.com>
2025-03-14 17:31:10 +0100

    Actividades Bucles

---
+++ js-bucles/main.ts
@@ -0,0 +1,29 @@
+const ejercicio1 = () =>{
+    for (let index = 0; index < 10; index++) {
+        console.log("I ❤️ code");
+    }
+
+    let i = 0;
+    while(i<10){
+        console.log("I ❤️ code");
+        i++;
+    }
+}
+ejercicio1();
+
+const ejercicio2 = () => {
+    let arr = ["a", "b", "c", "d", "e"];
+    arr.forEach(char => {
+        console.log(char);
+    })
+}
+ejercicio2();
+
+const ejercicio3 = () =>{
+    let numero =  5;
+    while(numero >= 0){
+        console.log(numero);
+        numero--;
+    }
+}
+ejercicio3();
