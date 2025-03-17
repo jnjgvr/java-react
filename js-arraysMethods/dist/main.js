958d3fcc292b708c5535eba07b329c84efb777a8
jnjgvr <jnjgvr@gmail.com>
2025-03-17 04:16:44 +0100

    Actividades Array Methods JS

--- js-arraysMethods/dist/main.js
+++ js-arraysMethods/dist/main.js
@@ -1,54 +1,48 @@
+"use strict";
 const ejercicio1 = () => {
-    const arr: string[] = ['🍔', '🌯', '🍣', '🍕', '🍜', '🍱', '🍙', '🍘', '🥑'];
+    const arr = ['🍔', '🌯', '🍣', '🍕', '🍜', '🍱', '🍙', '🍘', '🥑'];
     // 🍺
-
     const index = arr.indexOf('🍕');
-    if(index !== -1){
-        arr.splice(index + 1, arr.length - (index + 1), ...Array(arr.length - (index +1)).fill('🍺'));
+    if (index !== -1) {
+        arr.splice(index + 1, arr.length - (index + 1), ...Array(arr.length - (index + 1)).fill('🍺'));
     }
     return arr;
-}
+};
 console.log(ejercicio1());
-
 const ejercicio2 = () => {
-    const arr = ['🍕','🍕', '🍍', '🍕', '🍕'];
+    const arr = ['🍕', '🍕', '🍍', '🍕', '🍕'];
     return arr.includes('🍍');
-}
+};
 console.log(ejercicio2());
-
 const ejercicio3 = () => {
-    const arr = ['🍕','🍕', '🍍', '🍕', '🍕'];
+    const arr = ['🍕', '🍕', '🍍', '🍕', '🍕'];
     return arr.filter((icono) => '🍍' !== icono);
-}
+};
 console.log(ejercicio3());
-
 const ejercicio4 = () => {
     const arr = ['🍓', '🍋', '🍓', '🍋', '🍓'];
     // 🍄
     return arr.map(icono => '🍓' === icono ? '🍄' : icono);
-}
+};
 console.log(ejercicio4());
-
 const ejercicio5 = () => {
     const arr = ['🌶️', '🥛', '🌶️', '🥛', '🌶️', '🥛'];
     //🥵
     arr.map((icono, index, array) => {
-        if(icono === '🌶️'){
-            array.splice(index+1, 0, '🥵');
+        if (icono === '🌶️') {
+            array.splice(index + 1, 0, '🥵');
         }
-    })
-
+    });
     return arr;
-}
+};
 console.log(ejercicio5());
-
 const ejercicio6 = () => {
     const arr = ['🎴', '🎴', '🎴', '🃏', '🎴', '🎴', '🎴'];
     return arr.flatMap((icono, index, array) => {
-        if(icono !== '🃏' && array.at(index+1) === '🎴'){
+        if (icono !== '🃏' && array.at(index + 1) === '🎴') {
             return [icono, '🃏'];
         }
         return icono;
     });
-}
+};
 console.log(ejercicio6());
